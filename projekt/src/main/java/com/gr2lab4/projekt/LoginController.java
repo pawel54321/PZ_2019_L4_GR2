2a23c70677a262a0d6094ba4f9cbb81782bb1847
marczin <marcinrosol1997@gmail.com>
2019-04-29 23:47:38 +0200

    update login case

--- projekt/src/main/java/com/gr2lab4/projekt/LoginController.java
+++ projekt/src/main/java/com/gr2lab4/projekt/LoginController.java
@@ -48,7 +48,8 @@ public class LoginController {
     void LoginAction(ActionEvent e) throws IOException {
         String login = LoginField.getText().toString();
         String passwd = PasswordField.getText().toString();
-
+        String typ = "";
+        
         System.out.println(login + " " + passwd);
 
         if (MainApp.instance.appCfg.pracownicy.size() == 0) {
@@ -59,90 +60,60 @@ public class LoginController {
 
                     MainApp.instance.appCfg.setUser(tempPracownik);
                     
+                    typ = tempPracownik.getStanowisko().toString().toLowerCase();
                     
-                    // sprawdzic typ konta i otworzyc odpowiednie okno
-                    
-                    
-                    
-                    
-                    System.out.println("ZALOGOWANO");
+              
+
                     break;
                 }
             }
+            
+            Parent view2;
+            Scene scene2;
+            Stage window;
+            
+            switch (typ) {
+           
+            
+			case "admin":
+				 System.out.println("ZALOGOWANO admin");
+		          view2 = FXMLLoader.load(getClass()
+		                 .getResource("/fxml/AdminView.fxml")); 
+		                  scene2 = new Scene(view2);
+		                 window = (Stage)((Node)e.getSource()).getScene().getWindow(); 
+		                 window.setScene(scene2);  
+		                 window.show();
+				break;
+			case "manager":
+				
+				 System.out.println("ZALOGOWANO manager");
+				 
+		          view2 = FXMLLoader.load(getClass()
+			                 .getResource("/fxml/ManagerView.fxml")); 
+			                  scene2 = new Scene(view2);
+			                 window = (Stage)((Node)e.getSource()).getScene().getWindow(); 
+			                 window.setScene(scene2);  
+			                 window.show();
+				break;
+			case "pracownik":
+				 System.out.println("ZALOGOWANO pracownik");
+				 
+		          view2 = FXMLLoader.load(getClass()
+			                 .getResource("/fxml/PracownikView.fxml")); 
+			                  scene2 = new Scene(view2);
+			                 window = (Stage)((Node)e.getSource()).getScene().getWindow(); 
+			                 window.setScene(scene2);  
+			                 window.show();
+			                 
+				break;
+				
+				default:
+					System.out.println("default error");
+					break;
+			}
         }
-
-        // select do bazy danych z zapytaniem czy istnieje taki rekord
-        // zwrocic z bazy danych stanowisko jezeli istnieje rekord
-        // przelaczyc go do odpowiedniego widoku
-        // ustawic konfiguracje 
-        /* 
-         if((LoginField.getText().equals(aL)) 
-         && PasswordField.getText().equals(aP)){
-            
-         System.out.println("ADMIN");
-            
-            
-         MainApp.instance.appCfg.imie = "admin";
-         System.out.println("imie set");
-         MainApp.instance.appCfg.nazwisko ="admin";
-         System.out.println("nazwisko set");
-         MainApp.instance.appCfg.setAccountType(AccountType.ADMIN);
-         System.out.println("type set");
-            
-         MainApp.instance.dbLogger.setData();
-            
-            
-         Parent view2 = FXMLLoader.load(getClass()
-         .getResource("/fxml/AdminView.fxml")); 
-         Scene scene2 = new Scene(view2);
-         Stage window = (Stage)((Node)e.getSource()).getScene().getWindow(); 
-         window.setScene(scene2);  
-         window.show();
-
-         MainApp.instance.dbLogger.saveLog("logowanie do systemu");
-            
-         }else if((LoginField.getText().equals("manager")) 
-         && PasswordField.getText().equals("manager")){
-            
-         System.out.println("MANAGER");
-            
-         MainApp.instance.appCfg.imie = "manager";
-         MainApp.instance.appCfg.nazwisko ="manager";
-         MainApp.instance.appCfg.setAccountType(AccountType.MANAGER);
-            
-         Parent view2 = FXMLLoader.load(getClass().getResource("/fxml/ManagerView.fxml")); 
-         Scene scene2 = new Scene(view2);
-         Stage window = (Stage)((Node)e.getSource()).getScene().getWindow(); 
-         window.setScene(scene2);  
-         window.show();
-            
-           
-    
-            
-         }else{ //obecnie to nie jest bezpieczne wiec tutaj potem trzeba bedzie dodac warunek ze ma prawa do pracownika a nie na pale logowac
-            
-         System.out.println("PRACOWNIK");
-            
-         MainApp.instance.appCfg.imie = "pracownik";
-         MainApp.instance.appCfg.nazwisko ="pracownik";
-         MainApp.instance.appCfg.setAccountType(AccountType.PRACOWNIK);
-            
-         Parent view2 = FXMLLoader.load(getClass()
-         .getResource("/fxml/PracownikView.fxml")); 
-         Scene scene2 = new Scene(view2);
-         Stage window = (Stage)((Node)e.getSource()).getScene().getWindow(); 
-         window.setScene(scene2);  
-         window.show();
-            
-           
-            
-         } */
-// to jest na sztywniutko logowanie do admina potem sie tam zmnieni
-        //cały czas sztywniutko mordo
-        //TODO
-        //pobrac wartosci
-        //sprawdzic jaki maja status w bd (admin manager, pracownik)
-        //otworzyc odpowiednia dlla niego scene
+        
+        
     }
 
     @FXML
